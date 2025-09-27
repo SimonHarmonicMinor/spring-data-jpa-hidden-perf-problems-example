693d28732db7de953885906df872721766596672
Semyon Kirekov <semyon@kirekov.com>
2025-09-27 13:14:06 +0300

    fix FarmServiceTest

--- src/main/java/com/example/demo/domain/Farm.java
+++ src/main/java/com/example/demo/domain/Farm.java
@@ -1,15 +1,20 @@
 package com.example.demo.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = PROTECTED)
 public class Farm {
     @Id
     @GeneratedValue(strategy = IDENTITY)
@@ -20,10 +25,17 @@ public class Farm {
     private String description;
 
     @OneToMany(mappedBy = "farm")
-    private Set<Chicken> chickens;
+    private List<Chicken> chickens;
 
     @OneToMany(mappedBy = "farm")
     private List<Cow> cows;
+
+    public Farm(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.chickens = new ArrayList<>();
+        this.cows = new ArrayList<>();
+    }
 
     public void update(String name, String description) {
         this.name = name;
--- src/test/java/com/example/demo/service/FarmServiceTest.java
+++ src/test/java/com/example/demo/service/FarmServiceTest.java
@@ -1,39 +1,35 @@
-package com.example.demo.repo;
+package com.example.demo.service;
 
-import com.example.demo.service.AccessService;
-import com.example.demo.service.FarmService;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import com.example.demo.PgTestContainersConfig;
+import com.example.demo.domain.Farm;
+import com.example.demo.repo.FarmRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestComponent;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.transaction.support.TransactionSynchronization;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.time.Duration;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+@Testcontainers
 @SpringBootTest
-@Import({FarmService.class})
-class FarmRepositoryTest {
-
-    @Autowired
-    private FarmRepository farmRepository;
+@Import({FarmService.class, PgTestContainersConfig.class})
+@Slf4j
+class FarmServiceTest {
     @MockitoBean
     private AccessService accessService;
+    @Autowired
+    private FarmRepository farmRepository;
     @Autowired
     private FarmService farmService;
 
     @Test
-    void test() {
+    void logConnectionAcquiring() {
         when(accessService.hasAccessToUpdateFarm(any(), any())).thenAnswer(
                 invocationOnMock -> {
                     Thread.sleep(2000);
@@ -41,12 +37,16 @@ class FarmRepositoryTest {
                 }
         );
 
+        farmRepository.save(new Farm("name", "desc"));
+
+        log.info("Starting business code");
         farmService.updateFarm(
                 UUID.randomUUID(),
                 1L,
                 "new name",
                 "new description"
         );
+        log.info("Finishing business code");
     }
 
 }
